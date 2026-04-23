dfc9aad605fc4e0dfbfe0a38d4b4937e4d5d9906
kingsley <kingsleykay26@gmail.com>
2026-04-23 19:00:10 +0100

    feat(main): scroll to top on route change

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { StrictMode } from "react";
+import ScrollToTop from "./components/ScrollToTop";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -7,6 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>,
